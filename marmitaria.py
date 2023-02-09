2a311e45f21589152bfc0fe49fd12340be8c80e5
fs-allan <124394237+fs-allan@users.noreply.github.com>
2023-02-09 17:12:44 -0300

    Add files via upload

---
+++ marmitaria.py
@@ -0,0 +1,14 @@
+print ('Gil Marmitas agradece seu contato!')
+nome = input ('Qual o seu nome?')
+print ('Olá' , nome , '! Bem vindo a Gil Marmitas!')
+print ('Cardápio do dia é Churrasco, Frango, Lasanha, Panqueca')
+pedido = input ('Qual é o seu pedido?')
+tamanho = input ('Gostaria do tamanho P ou M?')
+print ('Confirma seu pedido de' , pedido , 'no tamanho' , tamanho , '?')
+
+ 
+
+
+
+
+
